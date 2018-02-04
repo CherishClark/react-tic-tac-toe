17adefb5d0d49127f58477668cbafc729719e1ad
CherishClark <cherish.g.clark@gmail.com>
2018-02-03 17:36:05 -0700

    created a dynamic list to show move history

--- src/index.js
+++ src/index.js
@@ -57,7 +57,7 @@ class Game extends React.Component {
   handleClick(i) {
     const history = this.state.history;
     const current = history[history.length-1];
-    const squares = this.state.squares.slice();
+    const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
@@ -73,6 +73,17 @@ class Game extends React.Component {
     const history = this.state.history
     const current = history[history.length-1];
     const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ?
+      'Go to move #' + move :
+      'go to game start';
+      return (
+        <li>
+          <button onClick={() => this.jumpTo(move)}>(desc)</button>
+          </li>
+        ) ;
+    });
 
     let status;
     if (winner) {
@@ -91,7 +102,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
