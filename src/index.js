e85e56964181810ef6469bae641d024da66007f0
CherishClark <cherish.g.clark@gmail.com>
2018-02-03 17:09:33 -0700

    setting inital state in Game in order to save history:

--- src/index.js
+++ src/index.js
@@ -69,6 +69,15 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      xisNext: true,
+    };
+  }
   render() {
     return (
       <div className="game">
@@ -103,6 +112,8 @@ function calculateWinner(squares) {
   }
   return null;
 }
+
+
 
 // ========================================
 
